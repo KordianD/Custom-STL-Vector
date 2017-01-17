65d334798c8edca6778ecd0ef80c6d95b36fb420
Kordian D <kordiandrozd@gmail.com>
2017-01-17 23:51:36 +0100

    Added some functionality and improved some function.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+#include "Vector.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    Vector<int> first {1,2,3};
+
+    std::cout << first.front() << std::endl;
+    std::cout << first.back() << std::endl;
+    std::cout << first.capacity() << std::endl;
+    std::cout << first.size() << std::endl;
+    std::cout << (first == first) << std::endl;
+    std::cout << (first != first) << std::endl << std::endl;
+
+    Vector<int> second {4,5,6};
+    Vector<int> third(second);
+
+    for (int i = 0; i < third.size(); i++)
+        std::cout << third.front() << " ";
+
+
+
+
 }
